2d1f6351f7289d53ca5a77a4b4ad2dd63c0063ac
oskar <oskar@OscarMateos>
2020-06-04 11:58:00 +0200

    Terminado!!

--- src/main/java/com/mycompany/agendaagencias/SecondaryController.java
+++ src/main/java/com/mycompany/agendaagencias/SecondaryController.java
@@ -6,19 +6,36 @@
 package com.mycompany.agendaagencias;
 
 import com.mycompany.agendaagencias.entities.Agenciaspublicitarias;
+import com.mycompany.agendaagencias.entities.Ambito;
+import java.math.BigDecimal;
 import java.net.URL;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.util.StringConverter;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 /**
  * FXML Controller class
@@ -50,11 +67,20 @@ public class SecondaryController implements Initializable {
     @FXML
     private CheckBox checkBoxCobro;
     
+    @FXML
+    private ComboBox<Ambito> comboBoxAmbito;
+    
+    @FXML
+    private DatePicker datepickerFecha ;
+
+    
     
     
     private Pane  rootContactosView;
     @FXML
     private AnchorPane rootDetalleView;
+    
+
 
  
     private TableView tableViewPrevio;
@@ -69,6 +95,8 @@ public class SecondaryController implements Initializable {
     @FXML
    private void onActionButtonGuardar(ActionEvent event) {
        
+       //Boolean para ver si los datos son correctos
+       boolean errorFormato = false;
        //Volver a la vista del primary
        StackPane rootMain = (StackPane)rootDetalleView.getScene().getRoot();
        rootMain.getChildren().remove(rootDetalleView);      
@@ -82,7 +110,55 @@ public class SecondaryController implements Initializable {
        agencia.setTelefono(textFieldTelefono.getText());
        agencia.setSoporte(textFieldSoporte.getText());
        agencia.setProvincia(textFieldProvincia.getText());
-
+       
+       
+        //Objetos de tabla relacionada: Ambito
+        if(comboBoxAmbito.getValue() != null) {
+            agencia.setAmbito(comboBoxAmbito.getValue());
+        } else {
+            Alert alert = new Alert(AlertType.INFORMATION, "Debe indicar un ambito");
+            alert.showAndWait();
+            errorFormato = true;
+        }
+        
+        //PrecioHora
+        if(!textFieldHora.getText().isEmpty()) {
+            try {
+                agencia.setPreciohora(BigDecimal.valueOf(Double.valueOf(textFieldHora.getText()).doubleValue()));
+            } catch(NumberFormatException ex) {
+                errorFormato = true;
+                Alert alert = new Alert(AlertType.INFORMATION, "PrecioHora no válido");
+                alert.showAndWait();
+                textFieldHora.requestFocus();
+            }    
+        }
+        
+        //Trabajos Realizados
+        if(!textFieldTrabajosRealizados.getText().isEmpty()) {
+            try {
+                agencia.setTrabajosrealizados(Integer.valueOf(textFieldTrabajosRealizados.getText()));
+            } catch(NumberFormatException ex) {
+                errorFormato = true;
+                Alert alert = new Alert(AlertType.INFORMATION, "Número de hijos no válido");
+                alert.showAndWait();
+                textFieldTrabajosRealizados.requestFocus();
+            }
+        }       
+        //CobraDespServicio(boolean)
+        agencia.setCobradespservicio(checkBoxCobro.isSelected());
+        
+        //AñoCreacion (fecha)
+        if(datepickerFecha.getValue() != null) {
+            LocalDate localDate = datepickerFecha.getValue();
+            ZonedDateTime zonedDateTime = localDate.atStartOfDay(ZoneId.systemDefault());
+            Instant instant = zonedDateTime.toInstant();
+            Date date = Date.from(instant);
+            agencia.setAnnocreacion(date);
+        } else {
+            agencia.setAnnocreacion(null);
+        }
+       
+        // Guardar en la base de datos
         if(nuevaAgencia) {
             entityManager.persist(agencia);
         } else {
@@ -105,6 +181,8 @@ public class SecondaryController implements Initializable {
         TablePosition pos = new TablePosition(tableViewPrevio, numFilaSeleccionada, null);
         tableViewPrevio.getFocusModel().focus(pos);
         tableViewPrevio.requestFocus();
+        
+
         
     }
     
@@ -159,8 +237,76 @@ public class SecondaryController implements Initializable {
         textFieldEmail.setText(agencia.getEmail());
         textFieldSoporte.setText(agencia.getSoporte());
         textFieldISBN.setText(agencia.getIsbn());
-        // Falta implementar el código para el resto de controles
+        
+        
+        
+        // Paso 11
+        
+        //Cobro (boolean)
+        if (agencia.getCobradespservicio()!= null) {
+            checkBoxCobro.setSelected(agencia.getCobradespservicio());
+        }
+        
+        //Fecha
+        if (agencia.getAnnocreacion()!= null) {
+            Date date = agencia.getAnnocreacion();
+            Instant instant = date.toInstant();
+            ZonedDateTime zdt = instant.atZone(ZoneId.systemDefault());
+            LocalDate localDate = zdt.toLocalDate();
+            datepickerFecha.setValue(localDate);
+        }
+        
+        //preciohora
+        if (agencia.getPreciohora()!= null) {
+            textFieldHora.setText(agencia.getPreciohora().toString());
+        } 
+        
+        //TrabajosRealizados
+        if(agencia.getTrabajosrealizados()!= null) {
+            textFieldTrabajosRealizados.setText(agencia.getTrabajosrealizados().toString());
+        }
+        
+        //Busqueda en la BD para rellenar el ComboBox Ambito
+        Query queryAmbitoFindAll = entityManager.createNamedQuery("Ambito.findAll");
+        List listAmbito = queryAmbitoFindAll.getResultList();
+        comboBoxAmbito.setItems(FXCollections.observableList(listAmbito));
+        //En caso de que el objeto Persona tenga asignada alguna Provincia, 
+        //se deberá seleccionar para que se muestre directamente en el ComboBox.
+        if (agencia.getAmbito() != null) {
+            comboBoxAmbito.setValue(agencia.getAmbito());
+        }
+        
+        //Como se mostraran los datos en el comboBox
+        comboBoxAmbito.setCellFactory((ListView<Ambito> l) -> new ListCell<Ambito>() {
+            @Override
+            protected void updateItem(Ambito ambito, boolean empty) {
+                super.updateItem(ambito, empty);
+                if (ambito == null || empty) {
+                    setText("");
+                } else {
+                    setText(ambito.getCodigo() + "-" + ambito.getAmbito());
+                }
+            }
+        });
+        
+        //
+        comboBoxAmbito.setConverter(new StringConverter<Ambito>() {
+            @Override
+            public String toString(Ambito ambito) {
+                if (ambito == null) {
+                    return null;
+                } else {
+                    return ambito.getCodigo() + "-" + ambito.getAmbito();
+                }
+            }
+            @Override
+            public Ambito fromString(String userId) {
+                return null;
+            }
+        });
     }
+    
+    
     
     
 }
